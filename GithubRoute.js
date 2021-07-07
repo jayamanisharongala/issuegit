e301cd9070ec61fd2d4d932b197bc02a7c99359a
jayamanisharongala <84433722+jayamanisharongala@users.noreply.github.com>
2021-07-07 12:18:40 -0400

    Update GithubRoute.js

--- GithubRoute.js
+++ GithubRoute.js
@@ -1,9 +1,19 @@
-import React  from 'react'
+import React ,{useState ,useEffect} from 'react'
+import axios from 'axios'
 import {BrowserRouter as Router,Link,Route} from 'react-router-dom'
 import User from './User'
 function GithubRoute() {
-   
-        let issues=[{name:'issue1',id:1},{name:'issue2',id:2}]
+    let issues=[{name:'Issues1',id:1},{name:'Issues2',id:2},{name:'Issues3',id:3},{name:'Issues4',id:4},{name:'Issues5',id:5}]
+    const[issue,setIssue]=useState([])
+    console.log(issue)
+    useEffect(() => {
+            axios.get('https://api.github.com/repos/octocat/Hello-World/issues')
+            .then(response =>{
+                setIssue(response.data)
+                console.log(response)
+            } )
+           
+    },[])
         return (
             <div>
                 <Router>
@@ -11,11 +21,9 @@ function GithubRoute() {
                 {
                     issues.map((item) =>
                     <div><Link to={"/issues/"+item.id + "/" +item.name}>{item.name}</Link></div> 
-                    )
+                    )  
                 }
-                <Route path="/issues/:id/:name"><User /></Route>
-                 
-            
+                <Route path="/issues/:id/:name">< User /></Route>   
             </Router>
             </div>
         )
